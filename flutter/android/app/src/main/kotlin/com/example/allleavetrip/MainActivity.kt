c782b8f22177f49dd9f544a01de0999f0de714a9
dbstn1325 <dbstn1325@naver.com>
2022-08-14 01:48:05 +0900

    feat: 브로슈어 상세페이지 UI 구현 완료

---
+++ flutter/android/app/src/main/kotlin/com/example/allleavetrip/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.allleavetrip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:allleavetrip/screen/detail_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'allLeaveTrip Demo',
+    theme: ThemeData(
+      fontFamily: 'Pretendard',
+    ),
+    home: DetailPage(),
+  ));
+}
