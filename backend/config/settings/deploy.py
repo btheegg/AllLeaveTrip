f2dfcacd1c8b859bf4dad3f7dd58f4bcbe18c174
dbstn1325 <dbstn1325@naver.com>
2022-08-16 08:48:23 +0900

    [build] 환경 파일 수정

--- backend/config/settings/deploy.py
+++ backend/config/settings/deploy.py
@@ -26,7 +26,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "altdb",
+        'USER': "altdb",
+        'PASSWORD': read_secret('MYSQL_PASSWORD'),
+        'HOST': 'mysql',
+        'PORT': '3306',
     }
 }
--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -1,19 +1,11 @@
 import os
 from .base import *
 
-
-def read_secret(secret_name):
-    file = open('/run/secrets/' + secret_name)
-    secret = file.read()
-    secret = secret.rstrip().lstrip()
-    file.close()
-    return secret
-    
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-yp1&^4cag9sc)34y0i+os*!+%0bi=n+%acu4v2+t)7^cl!_may'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
