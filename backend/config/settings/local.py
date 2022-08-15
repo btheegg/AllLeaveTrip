5f0f4010913be14ac31bc75b0925d9e266f642ea
dbstn1325 <dbstn1325@naver.com>
2022-08-16 02:41:11 +0900

    [build] 플러터 빌드 파일 수정

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -1,11 +1,19 @@
 import os
 from .base import *
 
+
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+    
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yp1&^4cag9sc)34y0i+os*!+%0bi=n+%acu4v2+t)7^cl!_may'
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -5,6 +5,6 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: CoursePage(), theme: ThemeData(primaryColor: Color(0xff58BF81))),
+        home: DetailPage(), theme: ThemeData(primaryColor: Color(0xff58BF81))),
   );
 }
