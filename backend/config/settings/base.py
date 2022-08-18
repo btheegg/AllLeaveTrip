155450e3e7dc418fea2f176d4e581f3db4b87b1f
dbstn1325 <dbstn1325@naver.com>
2022-08-18 20:05:55 +0900

    [build] 빌드 파일 수정

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import pymysql
+pymysql.install_as_MySQLdb()
 from pathlib import Path\
 
 # for static file
